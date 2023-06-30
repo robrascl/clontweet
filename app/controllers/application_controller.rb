f323e87e67997e3f2912ae490b075fb369b355f1
robrascl <robras.cl@gmail.com>
2023-06-30 19:10:53 -0400

    version 1.1 mejorar visuales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    Pagy::DEFAULT[:items] = 9
+    Pagy::DEFAULT[:items] = 10
     include Pagy::Backend
     require 'pagy/extras/bootstrap'
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,8 @@
 class Tweet < ApplicationRecord
+    validates :description, presence: true
+    validates :username, presence: true
+
+
     include PgSearch::Model
 
     pg_search_scope :search_full_text,
